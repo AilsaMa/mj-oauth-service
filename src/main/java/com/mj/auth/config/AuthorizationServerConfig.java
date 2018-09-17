15a3bc3b4a30f1cfe882b274a6d8cc02c4472fd0
majun <majun@baba88.com>
2018-09-17 16:40:40 +0800

    redis有问题

--- src/main/java/com/mj/auth/config/AuthorizationServerConfig.java
+++ src/main/java/com/mj/auth/config/AuthorizationServerConfig.java
@@ -4,21 +4,17 @@ package com.mj.auth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.*;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -32,6 +28,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Autowired
     private TokenStore tokenStore;
+
+    @Autowired
+    private RedisConnectionFactory connectionFactory;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -55,22 +54,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public TokenStore tokenStore() {
         //使用内存的tokenStore
         return new InMemoryTokenStore();
-    }
-
-    public static PasswordEncoder createDelegatingPasswordEncoder() {
-        String encodingId = "bcrypt";
-        Map<String, PasswordEncoder> encoders = new HashMap<>();
-        encoders.put(encodingId, new BCryptPasswordEncoder());
-        encoders.put("ldap", new LdapShaPasswordEncoder());
-        encoders.put("MD4", new Md4PasswordEncoder());
-        encoders.put("MD5", new MessageDigestPasswordEncoder("MD5"));
-        encoders.put("noop", NoOpPasswordEncoder.getInstance());
-        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
-        encoders.put("scrypt", new SCryptPasswordEncoder());
-        encoders.put("SHA-1", new MessageDigestPasswordEncoder("SHA-1"));
-        encoders.put("SHA-256", new MessageDigestPasswordEncoder("SHA-256"));
-        encoders.put("sha256", new StandardPasswordEncoder());
-
-        return new DelegatingPasswordEncoder(encodingId, encoders);
+       /* RedisTokenStore redis = new RedisTokenStore(connectionFactory);
+        return redis;*/
     }
 }
